3008082bff33250ac97cd3e276e65bf5e6db7675
hellopygame <58726383+hellopygame@users.noreply.github.com>
2025-11-23 12:43:53 +0700

    Add files via upload

---
+++ website/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify, render_template
+import json
+
+
+app = Flask(__name__)
+
+data = {
+
+}
+
+@app.route('/')
+def hello():
+    return render_template("index.html") 
+
+
+
+@app.route("/test")
+def text():
+    return jsonify({
+        "data": "hello",
+        "data2": 1,
+        "data3": None
+    })
+
+@app.route("/test2", methods = ["POST"])
+def text2():
+    data = request.json
+
+
+    return "My text!!!!" + data["data"]
+
+@app.route("/test3", methods = ["POST"])
+def text3():
+    data = request.json
+    
+    with open("data.json", "w") as f:
+        json.dump(data, f, indent= 4)
+
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    return jsonify(data)
+
+@app.route("/saveData")
+def saveData():
+    pass
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
